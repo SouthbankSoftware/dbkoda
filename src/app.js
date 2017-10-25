3a974cb2db0e94d8cc31eddc9f90f2f34a252f47
Chris Trott <chris@southbanksoftware.com>
2017-10-26 09:54:39 +1100

    Added File > Preferences option

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@
  * @Author: Wahaj Shamim <wahaj>
  * @Date:   2017-07-21T09:26:47+10:00
  * @Email:  wahaj@southbanksoftware.com
- * @Last modified by:   guiguan
- * @Last modified time: 2017-10-19T12:37:12+11:00
+ * @Last modified by:   chris
+ * @Last modified time: 2017-10-23T15:44:46+11:00
  */
 
 import _ from 'lodash';
@@ -201,6 +201,13 @@ const saveFileAsInEditor = () => {
     activeWindow.webContents.send('command', 'saveFileAs');
   }
 };
+
+const openPreferences = () => {
+  const activeWindow = BrowserWindow.getFocusedWindow();
+  if (activeWindow) {
+    activeWindow.webContents.send('command', 'openPreferences');
+  }
+}
 
 // Create main window with React UI
 const createWindow = (url, options) => {
@@ -470,6 +477,12 @@ const setAppMenu = () => {
           accelerator: 'CmdOrCtrl+Shift+S',
           click() {
             saveFileAsInEditor();
+          },
+        },
+        {
+          label: 'Preferences',
+          click() {
+            openPreferences();
           },
         },
         { role: 'close' },
